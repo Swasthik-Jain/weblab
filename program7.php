5028d40d342d053fbaacdab36af37c6025d08ff6
Swasthik-Jain <swasthikjain1432@gmail.com>
2022-07-21 07:47:04 +0530

    commit

---
+++ program7.php
@@ -0,0 +1,17 @@
+<head>
+
+ <style>
+  p {
+   color:yellow;
+   font-size:90px;
+   position:absolute;
+   top: 40%;
+   left: 35%;
+   
+  }
+  body {
+   background-color:maroon;
+  }
+ </style>
+ <p> <?php echo date(" h: i : s A");?> </p>
+</head>
